24b19e53312df7479417eb0a06f3adc7b6ddbd77
Szymon <szymonh.dg@gmail.com>
2020-11-19 15:28:09 +0100

    added format for header

--- main.py
+++ main.py
@@ -7,22 +7,36 @@ fake = Faker()
 
 def random_data():
 
+    rand = random.randint(10, 100)
+
     workbook = xlsxwriter.Workbook('filename.xlsx')
     worksheet = workbook.add_worksheet()
 
     # creating different formats for data
-
     bold = workbook.add_format({'bold': 1})
     italic = workbook.add_format({'italic': 1})
     underline = workbook.add_format({'underline': 1})
     superscript = workbook.add_format({'font_script': 1})
     subscript = workbook.add_format({'font_script': 2})
+    red = workbook.add_format({'font_color': 'red'})
 
-    name_list = []
-    for _ in range(random.randint(10, 100)):
-        name_list.append(fake.name())
+    format_list = [bold, italic, underline, superscript, subscript, red]  # list of created formats
 
-    worksheet.write_column('A1', name_list)
+    # format for header
+    header_format = workbook.add_format()
+    header_format.set_font_size(18)
+    header_format.set_bold()
+    header_format.set_bg_color('green')
+    header_format.set_align('center')
+    header_format.set_align('vcenter')
+
+    header_list = ['No.', 'Name', 'Address']
+
+    for i in range(rand):
+        form = random.choice(format_list)
+        name = fake.name()
+        worksheet.write(0 + i, 0, name, form)
+
     workbook.close()
 
 
